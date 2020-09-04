d8076c136be28329263eef08c614f073953b7371
Joseph Idbensalah <joseph.idbensalah@gmail.com>
2020-09-04 14:41:34 +0100

    Updates

---
+++ precache-manifest.0ebcdfc0153bd4981324f3660078d776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9880e0ec69ed7067e14cc0c910bacd9",
+    "url": "/ReactJs-To-do-App/index.html"
+  },
+  {
+    "revision": "729f35e0d353bc6cca21",
+    "url": "/ReactJs-To-do-App/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "c6aa1ad6326d9d49f7d4",
+    "url": "/ReactJs-To-do-App/static/js/2.4f35d63c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJs-To-do-App/static/js/2.4f35d63c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729f35e0d353bc6cca21",
+    "url": "/ReactJs-To-do-App/static/js/main.61156218.chunk.js"
+  },
+  {
+    "revision": "9eaa1fa9369c4003d8e3",
+    "url": "/ReactJs-To-do-App/static/js/runtime-main.846ab67d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactJs-To-do-App/static/media/logo.ee7cd8ed.svg"
+  }
+]);
